a88ee2af24071d029fcc8a698204a6cce0b2550b
AKI-25 <abdelkefi.ismail@insat.u-carthage.tn>
2024-04-30 15:06:41 +0100

    fix: masked blog section

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -70,7 +70,7 @@ const config: Config = {
           position: 'left',
           label: 'Tutorial',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
+        // {to: '/blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/KronosOrg/kronos-core',
           label: 'GitHub',
@@ -126,10 +126,10 @@ const config: Config = {
         {
           title: 'More',
           items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
+            // {
+            //   label: 'Blog',
+            //   to: '/blog',
+            // },
             {
               label: 'GitHub',
               href: 'https://github.com/KronosOrg/kronos-core',
