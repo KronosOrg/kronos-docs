2e327de79a05ab4a5b45fd380d0a703eb855c819
AKI-25 <abdelkefi.ismail@insat.u-carthage.tn>
2024-04-30 17:23:04 +0100

    feat: added kronos card and changed description

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -60,7 +60,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/kronos-card.jpg',
     navbar: {
       title: 'Kronos',
       logo: {
